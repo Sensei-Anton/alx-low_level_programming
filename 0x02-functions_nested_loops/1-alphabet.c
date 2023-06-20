a042fc47c4439b9ea37c6858206897522ccaeb4f
Sensei-Anton <antonnjoku@gmail.com>
2023-06-20 16:08:50 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,13 @@
  *
  * Return: Always 0 (success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; i+++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
+}
