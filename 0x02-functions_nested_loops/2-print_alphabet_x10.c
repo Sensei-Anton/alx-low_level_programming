9cca824bd0518b6f67c4cba7dd9bf2a5b636065e
Sensei-Anton <antonnjoku@gmail.com>
2023-06-21 04:57:23 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,8 @@
 /**
  * print_alpahabet_x10 - Prints the alphabet 10 times.
  *
- * description: this contains code thet prints alphabets a to z 10x.
+ * description:
+ * Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0 (success)
  */
