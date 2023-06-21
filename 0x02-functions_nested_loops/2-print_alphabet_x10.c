9f327646887d7b58ba8daf0688a77020971e0354
Sensei-Anton <antonnjoku@gmail.com>
2023-06-21 04:44:12 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * print_alpahabet_x10 - Prints the alphabet 10 times.
- * Description: this contains code thet prints alphabets a to z 10x.
+ * description: this contains code thet prints alphabets a to z 10x.
  * Return: Always 0 (success)
  */
 void print_alphabet_x10(void)
+	// description alphabet printer
 {
 	int i;
 	int j;
