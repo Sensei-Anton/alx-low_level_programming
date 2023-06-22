b2a8d2ae352b60089539f619b2997700fef46462
Sensei-Anton <antonnjoku@gmail.com>
2023-06-22 12:33:38 -0700

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_sign - Prints the alphabet 10 times.
-* @n - character
+* @n: - character
 * description:
 * Write a function that prints 10 times the alphab.
 *
@@ -12,12 +12,12 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
-return 1;
+return (1);
 }
 else if (n == 0)
 {
 _putchar('0');
-return 0;
+return (0);
 }
 else
 {
